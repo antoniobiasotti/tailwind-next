a01505596ea0413c71d456db0b69ed63815be893
antoniobiasotti <abamorim36@gmail.com>
2024-05-01 11:10:38 -0300

    Just exploring

--- tailwind-next/src/app/page.tsx
+++ tailwind-next/src/app/page.tsx
@@ -6,9 +6,22 @@ export default function Home() {
         <li>Hello Tailwind</li>
         <li>Hello Tailwind</li>
       </ul>
-      <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
-        Sign In  
-      </button>    
+      <form className="bg-slate-700 flex flex-col justify-center items-center rounded-md">
+        <div className="bg-slate-600 p-5">
+          <div className="block">
+            <label>User</label>
+            <input className="placeholder: rounded-md"></input>
+          </div>
+          <label>Password</label>
+          <input className="placeholder: rounded-md"></input>
+        </div>
+        <footer className="bg-slate-500 justify-center">
+          <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+            Sign In  
+          </button>
+          <span>Forgot your password?</span>
+        </footer>
+      </form>    
     </div>
   )
 }
