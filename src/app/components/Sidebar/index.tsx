eb2455ddc5dbb8b951df61c76e2e2d54bf805d2a
Antonio <abamorim36@gmail.com>
2024-05-06 09:51:05 -0300

    add: input search div

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,11 +1,18 @@
 import { Logo } from './Logo'
+import { Search } from 'lucide-react'
 
 export function Sidebar() {
   return (
-    <aside className="px-5 py-8 border-r-zinc-200 border-2 border-fuchsia-600">
+    <aside className="px-5 py-8 space-y-6 border-r-zinc-200 border-2 border-fuchsia-600">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"></div>
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <Search className="s-5 text-zinc-500" />
+        <input
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+          placeholder="Search"
+        />
+      </div>
     </aside>
   )
 }
