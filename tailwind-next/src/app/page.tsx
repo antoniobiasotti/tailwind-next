cdf249e9bc92157e930000c6896b331c1276c4a8
Antonio <abamorim36@gmail.com>
2024-05-06 10:19:34 -0300

    justs exploring

--- tailwind-next/src/app/page.tsx
+++ tailwind-next/src/app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-300 h-screen">
-      <h1 className="flex items-center gap-3 font-bold text-3xl sm:text-5xl lg:text-6xl text-antoniobia hover:text-violet-400 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h1>
-      <ul role="list" className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-400">
+      <h1 className="flex items-center gap-3 font-bold text-3xl sm:text-5xl lg:text-6xl text-antoniobia hover:text-violet-400 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+        Hello Tailwind
+      </h1>
+      <ul
+        role="list"
+        className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-400"
+      >
         <li>Hello Tailwind</li>
         <li>Hello Tailwind</li>
       </ul>
@@ -14,14 +19,20 @@ export default function Home() {
             <label>Password</label>
             <input className="placeholder: rounded-md"></input>
           </div>
-          <footer className="bg-slate-700 justify-center">
-            <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
-              Sign In  
+          <footer className="flex flex-col gap-2 items-center bg-slate-700">
+            <button
+              disabled
+              className="px-2 py-2 border-[0.5px] rounded-md text-sky-500 font-semibold mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+            >
+              Sign In
             </button>
-            <span>Forgot your password?</span>
+            <span className="hover:underline">Forgot your password?</span>
+            <span className="hover:underline">
+              Don't have an account? Create one
+            </span>
           </footer>
         </div>
-      </form>    
+      </form>
     </div>
   )
 }
