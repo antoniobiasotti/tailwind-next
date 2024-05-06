3a7d18e6cb69e1a1dffc6dc85b8b53f243e87d3e
Antonio <abamorim36@gmail.com>
2024-05-06 08:32:37 -0300

    add: logo.svg component

---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import { Logo } from './Logo'
+
+export function Sidebar() {
+  return (
+    <aside className="px-5 py-8 border-r-zinc-200 border-2 border-fuchsia-600">
+      <Logo />
+    </aside>
+  )
+}
