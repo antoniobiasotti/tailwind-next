7abf16599a3cb7ed3dbc1017bfaec533035aa691
Antonio <abamorim36@gmail.com>
2024-05-06 08:48:58 -0300

    working: search input

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -4,6 +4,8 @@ export function Sidebar() {
   return (
     <aside className="px-5 py-8 border-r-zinc-200 border-2 border-fuchsia-600">
       <Logo />
+
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"></div>
     </aside>
   )
 }
