9cbf5de8b11135fbd2ec2e8c17234e80ed4ed817
antoniobiasotti <abamorim36@gmail.com>
2024-05-11 08:47:22 -0300

    creating mulitple tabs with radixUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import * as Tabs from '@radix-ui/react-tabs'
+import { SettingsTabs } from './components/SettingsTabs/index'
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
 
-      <Tabs.Root>
-        <Tabs.List>
-          <Tabs.Trigger></Tabs.Trigger>
-        </Tabs.List>
-      </Tabs.Root>
+      <SettingsTabs />
     </>
   )
 }
