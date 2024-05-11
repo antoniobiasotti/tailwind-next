7fae79bebda6518674d65d0a6d64d2a61c47dd15
antoniobiasotti <abamorim36@gmail.com>
2024-05-11 08:06:49 -0300

    working with Tabs primitive - radixui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import * as Tabs from '@radix-ui/react-tabs'
+
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <Tabs.Root>
+        <Tabs.List>
+          <Tabs.Trigger></Tabs.Trigger>
+        </Tabs.List>
+      </Tabs.Root>
     </>
   )
 }
